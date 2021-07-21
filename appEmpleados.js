e9be67921fd04e4cdc6d284b0fab5c209cff3412
rubensato <rubensato@gmail.com>
2021-07-21 09:47:47 -0300

    archivo JS - armado dinamico de fichas empleados

--- appEmpleados.js
+++ appEmpleados.js
@@ -6,8 +6,8 @@
 var cantidadListado = 12;
 
 
-// function climaActual(codigoCiudad) {
-    fetch('https://randomuser.me/api/?results='+ cantidadListado)
+// Consulto datos
+fetch('https://randomuser.me/api/?results='+ cantidadListado)
     .then(response => response.json())
     .then(data => {
         //todos los datos
@@ -27,5 +27,42 @@ var cantidadListado = 12;
         console.log('celular: ' + data.results[0].cell);
         console.log('foto: ' + data.results[0].picture.large);
 
+        //llamo a la funcion que me muestra el listado
+        mostrarListado(cantidadListado, data.results) 
+
     })
-// }
+
+function mostrarListado(cantidad, listaEmpleados) {
+    // Obtener la referencia del elemento body
+    var marcoListado = document.getElementById('mostrarListado');
+
+    
+    // Crea un elemento <div>
+    var listadoGeneral = document.createElement("div");
+    listadoGeneral.classList.add("listadoGenerado");
+    
+    //crea las fichas de los empleados
+    for (var i = 0; i < cantidad; i++) {
+        // Crea un elemento <div> y un elemento <tbody>
+        var fichas = document.createElement("div");
+        fichas.className = "fichaEmpleado";
+        
+        // Crea un elemento <div> y un elemento <tbody>
+        var fichasFoto = document.createElement("img");
+        fichasFoto.className = "imgEmpleado";
+        console.log(listaEmpleados[i].picture.large);
+        fichasFoto.src = listaEmpleados[i].picture.large;
+
+        fichas.appendChild(fichasFoto);
+
+        // agrega la hilera al final de la tabla (al final del elemento tblbody)
+        listadoGeneral.appendChild(fichas);
+    }
+
+    // appends <table> into 'marcoTablero'
+    marcoListado.appendChild(listadoGeneral);
+
+
+    
+
+}
